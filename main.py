14017b4f583161277a7513fdd28bbf8a9095ca55
kodarfour <kodarfour@gmail.com>
2023-12-19 18:15:42 -0500

    starter code for google sheets manipulation and gitignore for confidential api keys/ authentication tokens

---
+++ main.py
@@ -0,0 +1,14 @@
+import google.auth
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+print("Hello GCloudSync")
+
+
+# Kofi Testing
+
+# Kofi Testing
+
+# Zo Testing
+
+# Zo Testing
